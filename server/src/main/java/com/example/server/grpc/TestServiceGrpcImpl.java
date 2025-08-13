3cef182a481c738a9e63e23206684af358ed9e2b
Sarim Ansari <sarim@sarims-mac.local>
2025-08-14 02:16:38 +0530

    add server module

---
+++ server/src/main/java/com/example/server/grpc/TestServiceGrpcImpl.java
@@ -0,0 +1,41 @@
+package com.example.server.grpc;
+
+import com.example.proto.*;
+import com.example.server.service.LargePayloadService;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@GrpcService
+public class TestServiceGrpcImpl extends TestServiceGrpc.TestServiceImplBase {
+
+    // Make sure your gRPC client connects to the gRPC port (default: 9090), not the REST port (default: 8080).
+
+    @Autowired
+    private LargePayloadService largePayloadService;
+
+    @Override
+    public void getLargePayload(LargePayloadRequest request, StreamObserver<LargePayloadResponse> responseObserver) {
+        int sizeKb = request.getSize();
+        var dataList = largePayloadService.generateLargePayload(sizeKb);
+        LargePayloadResponse.Builder respBuilder = LargePayloadResponse.newBuilder();
+        for (com.example.server.model.LargeDataObject obj : dataList) {
+            respBuilder.addData(toProto(obj));
+        }
+        responseObserver.onNext(respBuilder.build());
+        responseObserver.onCompleted();
+    }
+
+    private LargeDataObject toProto(com.example.server.model.LargeDataObject obj) {
+        LargeDataObject.Builder builder = LargeDataObject.newBuilder()
+                .setId(obj.getId())
+                .setName(obj.getName())
+                .setDescription(obj.getDescription())
+                .addAllTags(obj.getTags())
+                .setNested(NestedObject.newBuilder()
+                        .setLevel(obj.getNested().getLevel())
+                        .setInfo(obj.getNested().getInfo())
+                        .build());
+        return builder.build();
+    }
+}
